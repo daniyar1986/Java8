217934b8fa8024c0c4675b712fb21c9d9e8c05f1
DMyrzakanov <Danntu1986#>
2020-01-09 15:24:14 +0600

    Count empty in List

--- src/stream/StreamFilter.java
+++ src/stream/StreamFilter.java
@@ -1,4 +1,11 @@
 package stream;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StreamFilter {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
+        System.out.println("Count empty in List"+strings.stream().filter(s -> s.isEmpty()).count());
+    }
 }
