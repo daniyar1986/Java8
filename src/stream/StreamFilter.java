34ca5d69b1b2e9e9e33165ab54cd54f11ef6e49b
DMyrzakanov <Danntu1986#>
2020-01-09 16:08:27 +0600

    Count String length=3

--- src/stream/StreamFilter.java
+++ src/stream/StreamFilter.java
@@ -6,7 +6,8 @@ import java.util.List;
 public class StreamFilter {
     public static void main(String[] args) {
         List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd", "", "jkl");
-        System.out.println("Count empty in List" + strings.stream().filter(s -> s.isEmpty()).count());
+        System.out.println("Count empty in List " + strings.stream().filter(s -> s.isEmpty()).count());
+        System.out.println("Count String length = 3 : " + strings.stream().filter(filter -> filter.length() == 3).count());
 
     }
 }
