2e9f9cdcb57c110a6fbb36b7613fa8491340615e
DMyrzakanov <Danntu1986#>
2020-01-09 16:04:51 +0600

    reformat

--- src/stream/StreamFilter.java
+++ src/stream/StreamFilter.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 public class StreamFilter {
     public static void main(String[] args) {
-        List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
-        System.out.println("Count empty in List"+strings.stream().filter(s -> s.isEmpty()).count());
+        List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd", "", "jkl");
+        System.out.println("Count empty in List" + strings.stream().filter(s -> s.isEmpty()).count());
+
     }
 }
