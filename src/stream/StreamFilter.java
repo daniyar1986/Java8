c16488ffc50dcbd64452bf3e51f420c6f69d094e
DMyrzakanov <Danntu1986#>
2020-01-10 11:48:13 +0600

    Stream mapToInt

--- src/stream/StreamFilter.java
+++ src/stream/StreamFilter.java
@@ -10,5 +10,35 @@ public class StreamFilter {
         System.out.println("Count empty in List " + strings.stream().filter(s -> s.isEmpty()).count());
         System.out.println("Count String length = 3 : " + strings.stream().filter(filter -> filter.length() == 3).count());
         System.out.println("Show not empty String" + strings.stream().filter(s -> s != "").collect(Collectors.toList()));
+
+        Person danik = new Person("Danik", 33);
+        Person moldir = new Person("Moldir", 29);
+        Person akmaral = new Person("Akmaral", 35);
+
+        List<Person> persons = Arrays.asList(danik, moldir, akmaral);
+        Person result1 = persons.stream()
+                .filter(person -> "Danik".equalsIgnoreCase(person.getName()))
+                .findAny()
+                .orElse(null);
+        System.out.println(result1);
+
+        Person result2 = persons.stream()
+                .filter((p) -> "Danik1".equalsIgnoreCase(p.getName()) && p.getAge() == 33)
+                .findAny()
+                .orElse(null);
+        System.out.println(result2);
+
+        persons.stream().map(Person::getName)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+
+        int sum = persons.stream()
+                .filter(person -> person.getName().length() > 0)
+                .mapToInt(value -> value.getAge())
+                .sum();
+        System.out.println(sum);
+
     }
+
+
 }
